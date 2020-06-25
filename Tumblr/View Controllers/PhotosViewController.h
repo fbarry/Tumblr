79b0e17b3c7b0f979584deb326ec8b95146bf29a
Fiona Barry <fbarry@fb.com>
2020-06-25 15:12:35 -0400

    Project completion

---
+++ Tumblr/View Controllers/PhotosViewController.h
@@ -0,0 +1,17 @@
+//
+//  PhotosViewController.h
+//  Tumblr
+//
+//  Created by Fiona Barry on 6/25/20.
+//  Copyright © 2020 fbarry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotosViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tumblr/Views/PhotoCell.h
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.h
+//  Tumblr
+//
+//  Created by Fiona Barry on 6/25/20.
+//  Copyright © 2020 fbarry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *postImageView;
+
+@end
+
+NS_ASSUME_NONNULL_END
